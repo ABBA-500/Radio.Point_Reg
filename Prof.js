98e6d1c0404534d99158f963b71a3e1cb002e74c
ABBA-500 <urs-dance@yandex.ru>
2023-12-18 20:11:30 +0300

    main

---
+++ Prof.js
@@ -0,0 +1,6 @@
+let ActiveUser = localStorage.getItem('ActiveUserId');
+//document.querySelector('.results').innerHTML = 'Hello World!';
+document.querySelector('.Username').innerHTML = JSON.parse(localStorage.getItem(ActiveUser)).username;
+document.querySelector('.Email').innerHTML = JSON.parse(localStorage.getItem(ActiveUser)).email;
+document.querySelector('.Pass').innerHTML = JSON.parse(localStorage.getItem(ActiveUser)).password;
+localStorage.removeItem("ActiveUserId");
